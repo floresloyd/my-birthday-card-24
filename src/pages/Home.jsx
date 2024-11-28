cd3cc08408bac16d69919a294a6d920198f22477
Loyd <us.flores.loyd@gmail.com>
2024-11-28 17:01:55 -0500

    Completed landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,36 @@
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
-
+import cakeGif from "../assets/pixel-cake.gif";
+import { useEffect, useState } from 'react';
 
 function Home() {
   const reroute = useNavigate(); // Hook used to reroute
+  const phrases = [ "Celebrating", "Loyd's 24th Birthday", "College Graduation", "Click Here"];
+  const [currentPhrase, setCurrentPhrase] = useState(phrases[0]);
+  const [index, setIndex] = useState(0); // Manage the index of phrases
 
   const handleValidationReroute = () => {
     reroute('/validate');
   };
 
- 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % phrases.length); // Cycle through phrases
+    }, 2000); // Change phrase every 2 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [phrases.length]); // No dependency on the `phrases` array itself
+
+  useEffect(() => {
+    setCurrentPhrase(phrases[index]);
+  }, [index, phrases]); // Update the displayed phrase when the index changes
 
   return (
-    <div>
-      <h1>cake</h1>
-      <h2> Rotating text hbd loyd | graduating b.s. cs | click here to </h2>
-      <button onClick={handleValidationReroute}> next page </button>
+    <div className='home-main-container'>
+      <img className='cake-gif' src={cakeGif} alt='cake-gif' />
+      <div className='rotating-text-board'>
+        <h2 onClick={handleValidationReroute} className="clickable-text">{currentPhrase}</h2>
+      </div>
     </div>
   );
 }
